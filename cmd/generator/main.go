6a508a3e063ac0985ca4fa6cf79f8805fc2400d5
Milana Din <milanadin@gmail.com>
2026-09-17 16:28:56 +0300

    feat: add transaction generator for bank A

---
+++ cmd/generator/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/milana-dev/postgres-clickhouse-etl/internal/generator"
+)
+
+func main() {
+	ctx := context.Background()
+	godotenv.Load()
+	dsn := os.Getenv("DATABASE_URL")
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatalf("unable to create connection pool: %v", err)
+	}
+	defer pool.Close()
+
+	bankID := os.Getenv("BANK_ID")
+
+	for {
+		t := generator.NewTransaction(bankID)
+		query := `INSERT INTO transactions (uuid, bank_id, sender_account, receiver_account, amount, currency, occurred_at, status)
+									VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+		tag, err := pool.Exec(ctx, query, t.UUID, bankID, t.SenderAccount, t.ReceiverAccount, t.Amount, t.Currency, t.Timestamp, t.Status)
+		if err != nil {
+			log.Printf("exec error: %v", err)
+		}
+		fmt.Printf("Успешно вставлено строк: %d\n", tag.RowsAffected())
+		time.Sleep(time.Second)
+	}
+}
---
+++ migrations/000001_create_transactions_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE transactions(
+	uuid UUID PRIMARY KEY,
+	bank_id VARCHAR(50),
+	sender_account VARCHAR(20),
+	receiver_account VARCHAR(20),
+	amount DECIMAL(18, 2),
+	currency VARCHAR(5),
+	occurred_at TIMESTAMP WITH TIME ZONE,
+	status VARCHAR(10)
+)
